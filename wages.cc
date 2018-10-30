622b604187b2cd07e26a27d7dc0fa254b72af07b
abukamail <31450643+abukamail@users.noreply.github.com>
2018-10-29 22:29:02 -0400

    Update wages.cc

--- wages.cc
+++ wages.cc
@@ -1,7 +1,7 @@
 /*
  *      Author: Nasseef Abukamail
  *   Date: 2018-09-05
- * Description: This program calculates wages for a single employee using hours and hourly rate.
+ * Description: Add Description
  */
 
 #include <iostream>
@@ -11,21 +11,18 @@ using namespace std;
 //function prototypes
 
 int main() {
-    //0 variable declarationssss
+    //0 variable declaration
     double hours = 0, rate, wages;
     //1 get the hours
     cout << "Enter the hours: ";
     cin >> hours;
-    if (hours < 0) {
-        cout << "Invalid number of hours" << endl;
-        exit(1);
-    }
+    
     //2 get the rate
     cout << "Enter the rate: ";
     cin >> rate;
-
+    
     //5 Calculate the wages
-    if (hours > 40) { //calculate with overtime
+    if (hours > 40) { //calculate with overtime    
         wages = (hours - 40) * 1.5 * rate + 40 * rate;
     }
     else {//calculate without overtime
@@ -36,6 +33,6 @@ int main() {
     cout << "Hours:  " << hours << endl;
     cout << "Rate:   " << rate << endl;
     cout << "Wages: $" << wages << endl;
-
+    
     return 0;
 }
